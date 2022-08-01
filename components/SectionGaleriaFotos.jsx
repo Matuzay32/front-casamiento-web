b01ba53e06ebdc6402d03d60f5f749d79d51b78a
linuxlite <eze.rey92@gmail.com>
2022-08-01 14:58:25 -0300

    seccion por comenzar el carrusel'

---
+++ components/SectionGaleriaFotos.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Flex, Box, Heading, Icon, useColorModeValue } from "@chakra-ui/react";
+import { FaCameraRetro } from "react-icons/fa";
+
+export default function SectionGaleriaFotos() {
+	return (
+		<Flex
+			textAlign={"center"}
+			pt={10}
+			justifyContent={"center"}
+			direction={"column"}
+			width={"full"}
+		>
+			<Box
+				height={"auto"}
+				width={{ base: "full", sm: "lg", lg: "xl" }}
+				margin={"auto"}
+			>
+				<Heading
+					py={5}
+					fontSize={48}
+					fontWeight={"bold"}
+					color={useColorModeValue("gray.700", "gray.50")}
+				>
+					Galeria de fotos
+				</Heading>
+			</Box>
+			<Box
+				display={"flex"}
+				justifyContent={"space-evenly"}
+				flexFlow={{ base: "column", sm: "wrap", lg: "wrap" }}
+				spacing={"2"}
+				mt={10}
+			></Box>
+			<Box>
+				<Icon w={"70px"} h={"70px"} as={FaCameraRetro}></Icon>
+			</Box>
+		</Flex>
+	);
+}
