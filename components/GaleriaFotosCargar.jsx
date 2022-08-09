df9d9db2d7d6c09b76933384a77e8906d37face9
linuxlite <eze.rey92@gmail.com>
2022-08-09 12:31:08 -0300

    haciendo el formulario para la carga de imagenes

---
+++ components/GaleriaFotosCargar.jsx
@@ -0,0 +1,185 @@
+import {
+	useColorModeValue,
+	Box,
+	Flex,
+	Stack,
+	Heading,
+	Text,
+	Container,
+	Input,
+	Button,
+	SimpleGrid,
+	Avatar,
+	AvatarGroup,
+	useBreakpointValue,
+	IconProps,
+	Icon,
+	FormLabel,
+	Image,
+	Textarea,
+	Select,
+	useDisclosure,
+	Modal,
+	ModalContent,
+	ModalHeader,
+	ModalBody,
+	ModalFooter,
+	ModalCloseButton,
+	ModalOverlay,
+} from "@chakra-ui/react";
+import { useRef, useState, useEffect } from "react";
+import { FiCamera, FiSave } from "react-icons/fi";
+import { FaCarSide } from "react-icons/fa";
+import {
+	FaInstagram,
+	FaTwitter,
+	FaYoutube,
+	FaShoppingCart,
+} from "react-icons/fa";
+import { useRouter } from "next/router";
+import { PillPity } from "pill-pity";
+
+export default function GaleriaFotosCargar() {
+	const patterFill = useColorModeValue("white", "brand.300");
+
+	const router = useRouter();
+	// console.log(router);
+	const { push } = router;
+	const [modalContent, setModalContent] = useState("");
+	const refForm = useRef();
+
+	const fetchPost = async () => {
+		setModalContent("");
+		const { current: form } = refForm;
+		const formData = new FormData(form);
+
+		//esto va a cargar las fotos en la carpeta uploads y devolver los nombres
+		// const namesPhotos = await postAllPhotos(formData);
+		const imagenesGaleria = { images: namesPhotos };
+
+		//esta es la funcion que va hacer el metodo post a la base de imagenes que va ir a la galeria
+		// createImagenesGaleriaFetch(imagenesGaleria);
+	};
+
+	const handleSumbit = (event) => {
+		const { current: form } = refForm;
+		// event.preventDefault()
+		const formData = new FormData(form);
+
+		fetchPost();
+
+		push("/");
+	};
+
+	return (
+		<PillPity
+			position={"relative"}
+			w={"full"}
+			h={"100vh"}
+			// backgroundSize={"cover"}
+			// backgroundPosition={"25% 50%"}
+			// backgroundImage={
+			// 	"url(https://images.unsplash.com/photo-1535185384036-28bbc8035f28?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80)"
+			// }
+			pattern={"architect"}
+			patternFill={patterFill}
+			patOpacity={0.5}
+			bgColor={300}
+			as={Box}
+		>
+			<Container
+				as={SimpleGrid}
+				maxW={"7xl"}
+				columns={{ base: 1, md: 2 }}
+				spacing={{ base: 10, lg: 32 }}
+				py={{ base: 10, sm: 20, lg: 32 }}
+			>
+				<Stack spacing={{ base: 10, md: 20 }}>
+					<Heading
+						color={1900}
+						lineHeight={1.1}
+						fontSize={{ base: "3xl", sm: "4xl", md: "5xl", lg: "6xl" }}
+					></Heading>
+
+					<Stack direction={"row"} spacing={4} align={"center"}></Stack>
+				</Stack>
+				<Stack
+					bg={"gray.50"}
+					rounded={"xl"}
+					p={{ base: 4, sm: 6, md: 8 }}
+					spacing={{ base: 8 }}
+					maxW={{ lg: "lg" }}
+				>
+					<Stack spacing={4}>
+						<Heading
+							fontSize={"4xl"}
+							fontWeight={500}
+							color={100}
+							fontFamily={"body"}
+							width={"100%"}
+							mb={"1rem"}
+							borderBottomWidth={{ base: "2px", md: "2px" }}
+							lineHeight={1.1}
+							// fontSize={{ base: "2xl", sm: "3xl", md: "4xl" }}
+						>
+							Cargar imagenes
+						</Heading>
+						<Text color={"gray.500"} fontSize={{ base: "sm", sm: "md" }}>
+							Esto sirve para que se carguen las fotos en la galeria simplemente
+							seleccione las que desea y búsquelas en su ordenador
+						</Text>
+					</Stack>
+					<Box as={"form"} mt={10} ref={refForm}>
+						<Stack spacing={4}>
+							{/* <form ref={refForm}> */}
+
+							<FormLabel
+								fontSize={"md"}
+								fontWeight={500}
+								color={100}
+								fontFamily={"body"}
+								width={"100%"}
+								mb={"1rem"}
+								lineHeight={1.1}
+							>
+								Cargue las imagenes para la galeria de fotos{" "}
+							</FormLabel>
+
+							<Input
+								required
+								type={"file"}
+								name="file"
+								placeholder="Upload Img"
+								bg={"gray.100"}
+								border={0}
+								color={"gray.500"}
+								_placeholder={{
+									color: "gray.500",
+								}}
+								multiple
+							/>
+
+							{/* </form> */}
+
+							<Button
+								onClick={(e) => handleSumbit(e)}
+								w={"full"}
+								mt={8}
+								bg={useColorModeValue("#151f21", "gray.900")}
+								color={"white"}
+								rounded={"2xl"}
+								_hover={{
+									transform: "translateY(-2px)",
+									boxShadow: "lg",
+									backgroundColor: "#d5b063",
+								}}
+							>
+								Sugerir
+							</Button>
+						</Stack>
+					</Box>
+				</Stack>
+			</Container>
+		</PillPity>
+	);
+}
