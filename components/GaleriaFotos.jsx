1b3e8da6045e9d2cb3203e6d8dbb3b8984894333
linuxlite <eze.rey92@gmail.com>
2022-08-17 17:06:05 -0300

    carrousel terminado a grandes razgos

--- components/GaleriaFotos.jsx
+++ components/GaleriaFotos.jsx
@@ -21,29 +21,14 @@ export default function GaleriaFotos() {
 			bg: "black",
 		},
 	};
-	const [slides, setSlides] = useState([
+	const [slides, setSlides] = useState([{}]);
+	const [defaultImages, SetdefaultImages] = useState([
 		{
-			nombre: "images/1.jpeg",
-		},
-		{
-			nombre: "images/2.jpeg",
-		},
-		{
-			nombre: "images/3.jpeg",
-		},
-		{
-			nombre: "images/4.jpeg",
-		},
-		{
-			nombre: "images/5.jpeg",
-		},
-		{
-			nombre: "images/6.jpeg",
+			nombre: "images/notFound.jpeg",
 		},
 	]);
 
 	useEffect(() => {
-		console.log(muestraTodasLasFotosGaleria());
 		muestraTodasLasFotosGaleria().then((res) => {
 			setSlides(res[0]?.imagenes);
 		});
@@ -80,69 +65,100 @@ export default function GaleriaFotos() {
 			alignItems="center"
 			justifyContent="center"
 		>
-			<Flex w="full" overflow="visible" pos="relative">
-				<Flex h="auto" w="full" {...carouselStyle}>
-					{slides?.map((slide, sid) => (
-						<Box
-							p={"30px"}
-							key={`slide-${sid}`}
-							boxSize="full"
-							shadow="md"
-							flex="none"
-						>
-							<Text
-								backgroundColor={"blackAlpha.800"}
-								boxShadow={"dark-lg"}
-								borderRadius="30px"
-								mb={"10px"}
-								color="white"
-								fontSize="xs"
-								fontWeight={900}
-								p="8px 12px"
-								pos="absolute"
-								top="3"
-							>
-								{sid + 1} / {slidesCount}
-							</Text>
-							<Image
-								src={`http://localhost:4000/galeria/uploads/${slide.nombre}`}
-								boxShadow={"dark-lg"}
-								alt="carousel image"
-								boxSize="full"
-								objectFit="cover"
-								cursor={"pointer"}
-								onClick={() => setSlide(sid)}
-							/>
-						</Box>
-					))}
+			{!slides || undefined ? (
+				<Flex w="full" overflow="visible" pos="relative">
+					<Flex h="auto" w="full" {...carouselStyle}>
+						{defaultImages?.map((slide, sid) => {
+							return (
+								<Box
+									p={"30px"}
+									key={`slide-${sid}`}
+									boxSize="full"
+									shadow="md"
+									flex="none"
+								>
+									<Image
+										src={slide.nombre}
+										boxShadow={"dark-lg"}
+										alt="carousel image"
+										boxSize="full"
+										objectFit="cover"
+										cursor={"pointer"}
+									/>
+								</Box>
+							);
+						})}
+					</Flex>
 				</Flex>
-				<Text {...arrowStyles} left="0" onClick={prevSlide}>
-					&#10094;
-				</Text>
-				<Text {...arrowStyles} right="0" onClick={nextSlide}>
-					&#10095;
-				</Text>
-				<HStack justify="center" pos="absolute" bottom="8px" w="full">
-					{Array.from({
-						length: slidesCount,
-					}).map((_, slide) => (
-						<Box
-							key={`dots-${slide}`}
-							cursor="pointer"
-							boxSize={["7px", null, "15px"]}
-							m="0 2px"
-							bg={currentSlide === slide ? "blackAlpha.800" : "blackAlpha.500"}
-							rounded="50%"
-							display="inline-block"
-							transition="background-color 0.6s ease"
-							_hover={{
-								bg: "blackAlpha.800",
-							}}
-							onClick={() => setSlide(slide)}
-						></Box>
-					))}
-				</HStack>
-			</Flex>
+			) : (
+				<Flex w="full" overflow="visible" pos="relative">
+					<Flex h="auto" w="full" {...carouselStyle}>
+						{slides?.map((slide, sid) => {
+							return (
+								<Box
+									p={"30px"}
+									key={`slide-${sid}`}
+									boxSize="full"
+									shadow="md"
+									flex="none"
+								>
+									<Text
+										backgroundColor={"blackAlpha.800"}
+										boxShadow={"dark-lg"}
+										borderRadius="30px"
+										mb={"10px"}
+										color="white"
+										fontSize="xs"
+										fontWeight={900}
+										p="8px 12px"
+										pos="absolute"
+										top="3"
+									>
+										{sid + 1} / {slidesCount}
+									</Text>
+									<Image
+										src={`http://localhost:4000/galeria/uploads/${slide.nombre}`}
+										boxShadow={"dark-lg"}
+										alt="carousel image"
+										boxSize="full"
+										objectFit="cover"
+										cursor={"pointer"}
+										onClick={() => setSlide(sid)}
+									/>
+								</Box>
+							);
+						})}
+					</Flex>
+					<Text {...arrowStyles} left="0" onClick={prevSlide}>
+						&#10094;
+					</Text>
+					<Text {...arrowStyles} right="0" onClick={nextSlide}>
+						&#10095;
+					</Text>
+					<HStack justify="center" pos="absolute" bottom="8px" w="full">
+						{Array.from({
+							length: slidesCount,
+						}).map((_, slide) => (
+							<Box
+								key={`dots-${slide}`}
+								cursor="pointer"
+								boxSize={["7px", null, "15px"]}
+								m="0 2px"
+								bg={
+									currentSlide === slide ? "blackAlpha.800" : "blackAlpha.500"
+								}
+								rounded="50%"
+								display="inline-block"
+								transition="background-color 0.6s ease"
+								_hover={{
+									bg: "blackAlpha.800",
+								}}
+								onClick={() => setSlide(slide)}
+							></Box>
+						))}
+					</HStack>
+				</Flex>
+			)}
 		</Flex>
 	);
 }
