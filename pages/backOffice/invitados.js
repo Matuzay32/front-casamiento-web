a6bce928b8389895a5cfe24672a41ba47358c083
linuxlite <eze.rey92@gmail.com>
2022-08-25 22:06:05 -0300

    la modificaciones de lista de invitados y lista de musica

--- pages/backOffice/invitados.js
+++ pages/backOffice/invitados.js
@@ -1,28 +1,56 @@
-import React from "react";
 import { Box, Center } from "@chakra-ui/react";
 import Spotify from "../../components/Spotify";
 import HeroBackOffice from "../../components/HeroBackOffice";
 import ContenidoListaInvitados from "../../components/ContenidoListaInvitados";
+import { React, useEffect, useState } from "react";
+import swal from "sweetalert";
+import { obtenerTokenYRol } from "../../constantes/tokenLogica";
 
 export default function listaMusica() {
+	const [tokenRol, setTokenRol] = useState();
+	useEffect(() => {
+		obtenerTokenYRol().then((x) => {
+			setTokenRol(x);
+			console.log(x);
+			if (!x) {
+				swal({
+					title: "Error",
+					text: "Tiene que ser administrador para poder acceder",
+					icon: "error",
+				});
+			}
+			if (x?.rol === "USER") {
+				swal({
+					title: "Error",
+					text: "Tiene que ser administrador para poder acceder",
+					icon: "error",
+				});
+			}
+		});
+		return () => {};
+	}, []);
 	return (
-		<Box
-			// backgroundImage="linear-gradient(white,  #f7d7ac, #B2936C)"
-			backgroundSize={"cover"}
-			backgroundPosition={"50% 50%"}
-			backgroundImage={"url('../images/fondoInvitaciones.jpg')"}
-		>
-			<Box>
-				<HeroBackOffice
-					colorParrafo={"white"}
-					color={"white"}
-					titulo={"Lista de invitados"}
-					parrafo={
-						"Esta es la lista de invitados confirmados para el casamiento"
-					}
-				></HeroBackOffice>
-			</Box>
-			<ContenidoListaInvitados></ContenidoListaInvitados>
-		</Box>
+		<>
+			{tokenRol && tokenRol.rol === "ADMIN" && tokenRol.token && (
+				<Box
+					// backgroundImage="linear-gradient(white,  #f7d7ac, #B2936C)"
+					backgroundSize={"cover"}
+					backgroundPosition={"50% 50%"}
+					backgroundImage={"url('../images/fondoInvitaciones.jpg')"}
+				>
+					<Box>
+						<HeroBackOffice
+							colorParrafo={"white"}
+							color={"white"}
+							titulo={"Lista de invitados"}
+							parrafo={
+								"Esta es la lista de invitados confirmados para el casamiento"
+							}
+						></HeroBackOffice>
+					</Box>
+					<ContenidoListaInvitados></ContenidoListaInvitados>
+				</Box>
+			)}
+		</>
 	);
 }
--- pages/backOffice/listaMusica.js
+++ pages/backOffice/listaMusica.js
@@ -1,28 +1,56 @@
-import React from "react";
 import { Box, Center } from "@chakra-ui/react";
 import Spotify from "../../components/Spotify";
 import ContenidoListaMusica from "../../components/ContenidoListaMusica";
 import HeroBackOffice from "../../components/HeroBackOffice";
+import { React, useEffect, useState } from "react";
+import swal from "sweetalert";
+import { obtenerTokenYRol } from "../../constantes/tokenLogica";
 
 export default function listaMusica() {
+	const [tokenRol, setTokenRol] = useState();
+	useEffect(() => {
+		obtenerTokenYRol().then((x) => {
+			setTokenRol(x);
+			console.log(x);
+			if (!x) {
+				swal({
+					title: "Error",
+					text: "Tiene que ser administrador para poder acceder",
+					icon: "error",
+				});
+			}
+			if (x?.rol === "USER") {
+				swal({
+					title: "Error",
+					text: "Tiene que ser administrador para poder acceder",
+					icon: "error",
+				});
+			}
+		});
+		return () => {};
+	}, []);
 	return (
-		<Box
-			backgroundSize={"cover"}
-			backgroundPosition={"50% 50%"}
-			backgroundImage={"url('../images/fondoSpotify.jpg')"}
-			// backgroundImage="linear-gradient(white,  teal, black)"
-		>
-			<Box>
-				<HeroBackOffice
-					colorParrafo={"white"}
-					color={"white"}
-					titulo={"Lista de canciones"}
-					parrafo={
-						"Estas son las sugerencias musicales que ponen los invitados para el casamiento, ideal para arma la playlist perfecta"
-					}
-				></HeroBackOffice>
-			</Box>
-			<ContenidoListaMusica></ContenidoListaMusica>
-		</Box>
+		<>
+			{tokenRol && tokenRol.rol === "ADMIN" && tokenRol.token && (
+				<Box
+					backgroundSize={"cover"}
+					backgroundPosition={"50% 50%"}
+					backgroundImage={"url('../images/fondoSpotify.jpg')"}
+					// backgroundImage="linear-gradient(white,  teal, black)"
+				>
+					<Box>
+						<HeroBackOffice
+							colorParrafo={"white"}
+							color={"white"}
+							titulo={"Lista de canciones"}
+							parrafo={
+								"Estas son las sugerencias musicales que ponen los invitados para el casamiento, ideal para arma la playlist perfecta"
+							}
+						></HeroBackOffice>
+					</Box>
+					<ContenidoListaMusica></ContenidoListaMusica>
+				</Box>
+			)}
+		</>
 	);
 }
