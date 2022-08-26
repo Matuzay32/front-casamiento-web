0359311ccff0d7fc0c4c1b7915dc1f637881b58f
linuxlite <eze.rey92@gmail.com>
2022-08-26 13:55:38 -0300

    galeria de imagenes terminada

--- constantes/galeria.js
+++ constantes/galeria.js
@@ -1,3 +1,5 @@
+import { obtenerTokenYRol } from "./tokenLogica";
+
 export const postAllPhotos = async (data) => {
 	const res = await fetch("http://localhost:4000/galeria/files", {
 		method: "POST",
@@ -8,11 +10,13 @@ export const postAllPhotos = async (data) => {
 };
 
 export const guardarNombreImagenes = async (datos) => {
+	const respuesta = await obtenerTokenYRol();
 	const res = await fetch(`http://localhost:4000/galeria`, {
 		method: "POST",
 		headers: {
 			Accept: "application/json",
 			"Content-Type": "application/json",
+			token: respuesta?.token,
 		},
 		body: JSON.stringify(datos),
 	});
