0d7013fa68f7f0f2c8150c674b1510627bda1c8f
linuxlite <eze.rey92@gmail.com>
2022-08-26 13:27:45 -0300

    funciona la carga del tiempo

--- constantes/fecha.js
+++ constantes/fecha.js
@@ -1,14 +1,18 @@
 //Get de fecha
+import { obtenerTokenYRol } from "./tokenLogica";
 
 export const fechasHoras = async () =>
 	(await fetch("http://localhost:4000/fecha")).json();
 
 export const cargaFechaHora = async (data) => {
+	const respuesta = await obtenerTokenYRol();
 	return await fetch("http://localhost:4000/fecha", {
 		method: "POST",
 		body: JSON.stringify(data),
 		headers: {
+			Accept: "application/json",
 			"Content-Type": "application/json",
+			token: respuesta?.token,
 		},
 	});
 };
